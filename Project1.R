2c31e607576dbe06e7a59cc5429cec51c4a94aea
mulligansarah <smulligan@sc.edu>
2025-11-13 20:38:46 -0500

    Add questions

--- Project1.R
+++ Project1.R
@@ -1,1 +1,23 @@
-data <- read.csv('~/Advanced Ecology 1/group2.csv')
+data <- read.csv('~/AdvancedEcologyWorkshop/group2.csv')
+#Section 1
+##1 – Use a discrete density-dependent and -independent population model to assess the population growth 
+##dynamics of any three species within the 10 most abundant species. What can you infer from both models 
+##(Modeli | Speciesi)? For each species, what is the most proper model to assess their growth? Why?
+
+##2 – Based on the selected models, how does lambda change across time?
+
+##3 – Starting with the average abundance in 2000, perform a simulation (N = 100 replicates) of population 
+##growth using the growth variability based on the abundance values from start to 1999 (i.e., use 100 samples 
+##of lambda from 1980 to 1999’s frequency distribution to project population beyond 2000). How many times 
+##are the real abundance values intercepted by the uncertainty bounds of the simulation (i.e., values within 
+##the 95% confidence intervals)? Briefly discussed the performance of the models and potential reasons for 
+##the results.
+
+#Section 2
+##1 – Does the sampling assigned to your group reach the asymptote of the species accumulation curve? Discuss results.
+
+##2 - Using the species that composed 75% of the abundance of species within the nektonic community, assess 
+##the annual trends of alpha and beta diversity, and the inverse Simpson diversity index.
+
+##3 – Using a community trajectory analysis, assess the annual stability of the fish community across the 
+##sites from the assigned basin and gear type. Describe and discuss the main results.
